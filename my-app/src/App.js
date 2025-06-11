5b2215437257b600b2260d569c0030dca2ed2c15
Autumn-Coding <78398232+Autumn-Coding@users.noreply.github.com>
2025-06-11 03:16:15 +0000

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './Layout/Layout';
-import { Home, HomeHeader } from './Home';
-import { About, AboutHeader } from './About';
+import { Home, HomeHeader } from './Pages/Home';
+import { About, AboutHeader } from './Pages/About';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter className="mainBody">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
+    <BrowserRouter>
+      <div className="mainBody">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
